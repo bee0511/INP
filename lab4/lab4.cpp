a5d0af0f3ca45a5df489670889cedf215970db5e
bee0511 <bee000092.cs10@nycu.edu.tw>
2023-10-26 17:15:08 +0800

    finish all

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -15,6 +15,7 @@ int main() {
     const char* port = "10314";
     const char* path_otp = "/otp?name=110550164";  // Updated student ID
     const char* path_upload = "/upload";
+    const char* output_file = "file_to_upload.txt"; // The file to upload
 
     // Create a socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -98,18 +99,28 @@ int main() {
         return 1;
     }
 
-const char* boundary = "WebKitFormBoundary7TMYhSONfkAM2z3a";
+const char* boundary = "----WebKitFormBoundaryA6blcZTkTIB37MaT";
 
-// Construct and send the POST request with the "verify" string
 stringstream post_request;
 post_request << "POST " << path_upload << " HTTP/1.1\r\n"
-             << "Host: " << server << "\r\n\r\n"
-             << "Content-Type: multipart/form-data; boundary=" << boundary << "\r\n\r\n"
+             << "Host: " << server << "\r\n"
+             << "Connection: close\r\n"
+             << "Content-Length: " << 298 << "\r\n"
+             << "Cache-Control: max-age=0\r\n"
+             << "Upgrade-Insecure-Requests: 1\r\n"
+             << "Origin: http://inp.zoolab.org:10314\r\n"
+             << "Content-Type: multipart/form-data; boundary=" << boundary << "\r\n"
+             << "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36\r\n"
+             << "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n"
+             << "Referer: http://inp.zoolab.org:10314/upload\r\n"
+             << "Accept-Encoding: gzip, deflate\r\n"
+             << "Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7\r\n\r\n"
              << "--" << boundary << "\r\n"
-             << "Content-Disposition: form-data; name=\"file\"; filename=\"test.txt\"\r\n"
+             << "Content-Disposition: form-data; name=\"file\"; filename=\"otp1.txt\"\r\n"
              << "Content-Type: text/plain\r\n\r\n"
              << verify << "\r\n"
              << "--" << boundary << "--\r\n";
+
 
 
     string request_upload = post_request.str();
@@ -118,6 +129,13 @@ post_request << "POST " << path_upload << " HTTP/1.1\r\n"
         close(post_sock);
         return 1;
     }
+
+    // // Send the verify string as part of the request body
+    // if (send(post_sock, verify.c_str(), verify.size(), 0) == -1) {
+    //     perror("Error sending verify string");
+    //     close(post_sock);
+    //     return 1;
+    // }
 
     // Receive and print the response
     while ((bytes_received = recv(post_sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
--- lab4/lab4_internal.cpp
+++ lab4/lab4_internal.cpp
@@ -11,11 +11,10 @@
 using namespace std;
 
 int main() {
-    const char* server = "inp.zoolab.org";
-    const char* port = "10314";
+    const char* server = "172.21.0.4";
+    const char* port = "10001";
     const char* path_otp = "/otp?name=110550164";  // Updated student ID
     const char* path_upload = "/upload";
-    const char* output_file = "verify.txt"; // The file to save the verify string
 
     // Create a socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -23,19 +22,10 @@ int main() {
         perror("Socket creation failed");
         return 1;
     }
-
-    // Resolve server's IP address
-    struct hostent* server_info = gethostbyname(server);
-    if (!server_info) {
-        perror("Error resolving host");
-        close(sock);
-        return 1;
-    }
-
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(atoi(port));
-    server_address.sin_addr.s_addr = *((unsigned long*)server_info->h_addr_list[0]); // Use the first address in the list
+    server_address.sin_addr.s_addr = inet_addr(server); // Use the provided IP addresst address in the list
 
     // Connect to the server
     if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
@@ -71,16 +61,13 @@ int main() {
         int j = i;
         while (buffer[j] != '=') {
             if (buffer[j] == '+') {
-                // verify.append("%2b");
                 verify.append("+");
                 j++;
                 continue;
             }
-            
             verify.push_back(buffer[j]);
             j++;
         }
-        // verify.append("%3d%3d");
         verify.append("==");
         break;
     }
@@ -88,51 +75,62 @@ int main() {
     memset(buffer, 0, sizeof(buffer)); // Clear the buffer
     close(sock);
 
-ofstream verifyFile(output_file);
-    if (verifyFile.is_open()) {
-        verifyFile << verify;
-        verifyFile.close();
-    } else {
-        cerr << "Error opening " << output_file << " for writing" << endl;
-        return 1;
-    }
-
-
-    // Append the "verify" to the "path_upload"
-    std::string path_upload_with_verify = "/verify?otp=" + verify;
-    cout << path_upload_with_verify << endl;
-    // Create a new socket for the GET request
-    int otp_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (otp_sock == -1) {
+    // Create a socket for the POST request
+    int post_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (post_sock == -1) {
         perror("Socket creation failed");
         return 1;
     }
 
     // Connect to the server again
-    if (connect(otp_sock, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
+    if (connect(post_sock, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
         perror("Connection failed");
-        close(otp_sock);
+        close(post_sock);
         return 1;
     }
 
-    // Construct and send the GET request to verify OTP
-    std::string request_verify = "GET " + path_upload_with_verify + " HTTP/1.1\r\n" +
-                              "Host: " + std::string(server) + "\r\n" +
-                              "Connection: close\r\n" +
-                              "\r\n";
+const char* boundary = "----WebKitFormBoundaryA6blcZTkTIB37MaT";
 
-    if (send(otp_sock, request_verify.c_str(), request_verify.size(), 0) == -1) {
-        perror("Error sending verify request");
-        close(otp_sock);
+stringstream post_request;
+post_request << "POST " << path_upload << " HTTP/1.1\r\n"
+             << "Host: " << server << "\r\n"
+             << "Connection: close\r\n"
+             << "Content-Length: " << 298 << "\r\n"
+             << "Cache-Control: max-age=0\r\n"
+             << "Upgrade-Insecure-Requests: 1\r\n"
+             << "Origin: http://inp.zoolab.org:10314\r\n"
+             << "Content-Type: multipart/form-data; boundary=" << boundary << "\r\n"
+             << "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36\r\n"
+             << "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n"
+             << "Referer: http://inp.zoolab.org:10314/upload\r\n"
+             << "Accept-Encoding: gzip, deflate\r\n"
+             << "Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7\r\n\r\n"
+             << "--" << boundary << "\r\n"
+             << "Content-Disposition: form-data; name=\"file\"; filename=\"otp1.txt\"\r\n"
+             << "Content-Type: text/plain\r\n\r\n"
+             << verify << "\r\n"
+             << "--" << boundary << "--\r\n";
+
+
+
+    string request_upload = post_request.str();
+    if (send(post_sock, request_upload.c_str(), request_upload.size(), 0) == -1) {
+        perror("Error sending upload request");
+        close(post_sock);
         return 1;
     }
 
-    while ((bytes_received = recv(otp_sock, buffer, sizeof(buffer), 0)) > 0) {
-        std::cout.write(buffer, bytes_received);
+    // Receive and print the response
+    while ((bytes_received = recv(post_sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[bytes_received] = '\0'; // Null-terminate the received data
+        std::cout << buffer;
     }
-    
-    memset(buffer, 0, sizeof(buffer)); // Clear the buffer
-    close(otp_sock);
+
+    if (bytes_received == -1) {
+        perror("Error receiving upload response");
+    }
+
+    close(post_sock);
 
     return 0;
 }
