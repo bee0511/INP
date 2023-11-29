867e0ec3e2152917247c52d970152ec71af8d2bb
bee0511 <bee000092.cs10@nycu.edu.tw>
2023-11-29 22:11:11 +0800

    [hw1] change makefile

--- homework1/demo.sh
+++ homework1/demo.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-set -x
+# set -x
 make builder
 docker-compose restart demo
 docker exec -it sw_tester /testcase/run.sh demo
+
+for i in $(seq 10); do
+    sleep 10
+    docker-compose restart demo
+done
